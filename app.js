dc34a0bd11023ac38b5daf27fb5fa3a93266bf7d
Sam <samjeffcoat@gmail.com>
2021-05-28 12:07:30 -0500

    adds middleware for our server

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const path = require("path")
 const app = express()
+app.use(express.urlencoded({extended: true}))
 
 
 //Express using PUG templating Engine, look for templates in the view folder
@@ -10,3 +11,19 @@ app.set("views", path.join(__dirname, "views"))
 // Initializing our server
 app.get('/', (req, res) => res.render("index"))
 app.listen(3000, () => console.log("Server Ready"))
+
+
+// Creating a post endpoing
+
+app.post("/", (req, res) => {
+    const {username, password} = req.body
+
+    if(!username || !password){
+        res.render("error", {
+            message: "Please set both username and password"
+        })
+        return
+    }
+    console.log(req.body, username, password)
+    res.end()
+})
