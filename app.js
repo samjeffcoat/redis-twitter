b58065c947a65700cd751b85b6da78b071812b4b
Sam <samjeffcoat@gmail.com>
2021-06-01 23:27:53 -0500

    project done for now

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const path = require("path");
-const bcrypt = require("bcrypt");
 const redis = require("redis");
+const bcrypt = require("bcrypt");
 const session = require("express-session");
-const RedisStore = require("connect-redis")(session);
-const app = express();
 const client = redis.createClient();
+const { promisify } = require("util");
+const { formatDistance } = require("date-fns");
+
+const app = express();
+
+const RedisStore = require("connect-redis")(session);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
@@ -21,29 +26,114 @@ app.use(
   })
 );
 
-//Express using PUG templating Engine, look for templates in the view folder
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
-// Initializing our server
-app.get("/", (req, res) => {
-  if (req.session.userid) {
-    client.hkeys("users", (err, users)=> {
-        console.log(users)
-        res.render("dashboard", {
-            users,
-        });
+const ahget = promisify(client.hget).bind(client);
+const asmembers = promisify(client.smembers).bind(client);
+const ahkeys = promisify(client.hkeys).bind(client);
+const aincr = promisify(client.incr).bind(client);
+const alrange = promisify(client.lrange).bind(client);
 
-    })
+app.get("/", async (req, res) => {
+  if (req.session.userid) {
+    const currentUserName = await ahget(
+      `user:${req.session.userid}`,
+      "username"
+    );
+    const following = await asmembers(`following:${currentUserName}`);
+    const users = await ahkeys("users");
+
+    const timeline = [];
+    const posts = await alrange(`timeline:${currentUserName}`, 0, 100);
+
+    for (post of posts) {
+      const timestamp = await ahget(`post:${post}`, "timestamp");
+      const timeString = formatDistance(
+        new Date(),
+        new Date(parseInt(timestamp))
+      );
+
+      timeline.push({
+        message: await ahget(`post:${post}`, "message"),
+        author: await ahget(`post:${post}`, "username"),
+        timeString: timeString,
+      });
+    }
+
+    res.render("dashboard", {
+      users: users.filter(
+        (user) => user !== currentUserName && following.indexOf(user) === -1
+      ),
+      currentUserName,
+      timeline,
+    });
   } else {
     res.render("login");
   }
 });
 
-// Creating a post endpoing
+app.get("/post", (req, res) => {
+  if (req.session.userid) {
+    res.render("post");
+  } else {
+    res.render("login");
+  }
+});
+
+app.post("/post", async (req, res) => {
+  if (!req.session.userid) {
+    res.render("login");
+    return;
+  }
+
+  const { message } = req.body;
+  const currentUserName = await ahget(`user:${req.session.userid}`, "username");
+  const postid = await aincr("postid");
+  client.hmset(
+    `post:${postid}`,
+    "userid",
+    req.session.userid,
+    "username",
+    currentUserName,
+    "message",
+    message,
+    "timestamp",
+    Date.now()
+  );
+  client.lpush(`timeline:${currentUserName}`, postid);
+
+  const followers = await asmembers(`followers:${currentUserName}`);
+  for (follower of followers) {
+    client.lpush(`timeline:${follower}`, postid);
+  }
+
+  res.redirect("/");
+});
+
+app.post("/follow", (req, res) => {
+  if (!req.session.userid) {
+    res.render("login");
+    return;
+  }
+
+  const { username } = req.body;
+
+  client.hget(
+    `user:${req.session.userid}`,
+    "username",
+    (err, currentUserName) => {
+      client.sadd(`following:${currentUserName}`, username);
+      client.sadd(`followers:${username}`, currentUserName);
+    }
+  );
+
+  res.redirect("/");
+});
 
 app.post("/", (req, res) => {
   const { username, password } = req.body;
+
   if (!username || !password) {
     res.render("error", {
       message: "Please set both username and password",
@@ -54,21 +144,18 @@ app.post("/", (req, res) => {
   const saveSessionAndRenderDashboard = (userid) => {
     req.session.userid = userid;
     req.session.save();
-    client.hkeys("users", (err, users)=> {
-        console.log(users)
-        res.render("dashboard", {
-            users,
-        });
-
-    })
+    res.redirect("/");
   };
 
   const handleSignup = (username, password) => {
     client.incr("userid", async (err, userid) => {
       client.hset("users", username, userid);
+
       const saltRounds = 10;
-      const hash = await brcrypt.hash(password, saltRounds);
+      const hash = await bcrypt.hash(password, saltRounds);
+
       client.hset(`user:${userid}`, "hash", hash, "username", username);
+
       saveSessionAndRenderDashboard(userid);
     });
   };
@@ -82,49 +169,20 @@ app.post("/", (req, res) => {
         res.render("error", {
           message: "Incorrect password",
         });
+        return;
       }
     });
   };
 
   client.hget("users", username, (err, userid) => {
     if (!userid) {
-      //user does not exist, signup procedure
+      //signup procedure
       handleSignup(username, password);
     } else {
-      //user exists, login procedure
+      //login procedure
       handleLogin(userid, password);
     }
   });
 });
 
-// adding a post route
-app.get("/post", (req, res) => {
-  if (req.session.userid) {
-    res.render("post");
-  } else {
-    res.render("login");
-  }
-});
-
-app.post("/post", (req, res) => {
-    if(!req.session.userid){
-        res.render("login")
-        return
-    }
-
-    const {message} = req.body
-
-    client.incr("postid", async(err, postid) => {
-        client.hmset(
-            `post:${postid}`,
-            "userid",
-            req.session.userid,
-            "message",
-            message,
-            "timestamp",
-            Date.now()
-        )
-        res.render("dashboard")
-    })
-})
-app.listen(3000, () => console.log("Server Ready"));
+app.listen(3000, () => console.log("Server ready"));
